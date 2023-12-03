e4c91f56fe73445162e59bf203b4558cd0d27f9f
riv06 <152757843+riv06@users.noreply.github.com>
2023-12-02 21:41:59 -0600

    primera modificacion

---
+++ Hospital/src/main/java/com/mycompany/hospital/Hospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hospital;
+
+/**
+ *
+ * @author juan_
+ */
+public class Hospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
